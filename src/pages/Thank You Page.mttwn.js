98fb85f29e2e07906b228e3e165375debe6d621b
Jas3 <145662412+JasMyne14@users.noreply.github.com>
2024-09-23 16:29:35 +0800

    Designed font and layout in Thank You Page.mttwn.js

--- src/pages/Thank You Page.mttwn.js
+++ src/pages/Thank You Page.mttwn.js
@@ -3,7 +3,7 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    // Design layout and colors
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
