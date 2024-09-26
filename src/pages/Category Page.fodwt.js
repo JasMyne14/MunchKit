ce0f447c845e7871c39ab472dc6110d3eaee68c6
Tara <167838799+TaraniDevi@users.noreply.github.com>
2024-09-27 00:32:26 +0800

    Update Category Page.fodwt.js

--- src/pages/Category Page.fodwt.js
+++ src/pages/Category Page.fodwt.js
@@ -4,7 +4,7 @@
 $w.onReady(function () {
     // Write your JavaScript here
     // Designed category page 
-    // Described each product
+    // Described all 5 of the product precisely
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
